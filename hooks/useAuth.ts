52de50b78a6e3ce7ac1ca2bcbf01ed0987ce5b24
DamianBigos <damianbigos1500@gmail.com>
2023-02-09 22:39:29 +0100

    add login, register function in useAuth hook

---
+++ hooks/useAuth.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function useAuth() {
+  const register = async () => {};
+  
+  const login = async () => {};
+
+  const logout = async () => {};
+
+  const getUser = async () => {};
+
+  return { login };
+}
