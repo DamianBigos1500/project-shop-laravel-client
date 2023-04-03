a8b1500a9c981c967626b2c1410d61a10fb308b1
DamianBigos <damianbigos1500@gmail.com>
2023-04-03 18:09:28 +0200

    repair vercel bug

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,4 +1,6 @@
 import Axios from 'axios';
+// process.env.NEXT_PUBLIC_BACKEND_URL
+const baseUrl = 'https://project-shop-laravel-server-main.vercel.app';
 
 const option = {
   headers: {
@@ -9,12 +11,12 @@ const option = {
 };
 
 const axios = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: baseUrl,
   ...option,
 });
 
 export const axiosFormData = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: baseUrl,
   headers: {
     'X-Requested-With': 'XMLHttpRequest',
   },
