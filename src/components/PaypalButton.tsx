dec00b2cc4a8686022558989d6f5a8c93e12ef63
DamianBigos <damianbigos1500@gmail.com>
2023-04-03 18:37:12 +0200

    repair vercel bug

--- src/components/PaypalButton.tsx
+++ src/components/PaypalButton.tsx
@@ -4,13 +4,12 @@ import {
   PayPalScriptProvider,
   ReactPayPalScriptOptions,
 } from '@paypal/react-paypal-js';
-import { SCRIPT_ID } from '@paypal/react-paypal-js/dist/types/constants';
 import React from 'react';
 
-const initialOptions: ReactPayPalScriptOptions = {
-  'client-id': `${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}`,
-  [SCRIPT_ID]: SCRIPT_ID,
-};
+// const initialOptions: any = {
+//   'client-id': `${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}`,
+//   [SCRIPT_ID]: SCRIPT_ID,
+// };
 
 type propsType = {
   createOrderData(): any;
@@ -23,47 +22,48 @@ export default function PaypalButton({
   setOrderDataPaypal,
   totalValue = 10,
 }: propsType) {
-  return (
-    <PayPalScriptProvider options={initialOptions}>
-      <PayPalButtons
-        style={{
-          color: 'silver',
-          layout: 'horizontal',
-          height: 48,
-          tagline: false,
-          shape: 'pill',
-        }}
-        createOrder={(data, actions) => {
-          return actions.order.create({
-            purchase_units: [
-              {
-                description: 'description',
-                amount: {
-                  value: String(totalValue),
-                },
-              },
-            ],
-          });
-        }}
-        onApprove={async (data, actions: any) => {
-          console.log('approve', data);
-          try {
-            const newOrder = await createOrderData();
-            await actions.order.capture();
-            await setOrderDataPaypal(newOrder.id);
-          } catch (error) {}
+  return null
+  // return (
+    // <PayPalScriptProvider options={initialOptions}>
+    //   <PayPalButtons
+    //     style={{
+    //       color: 'silver',
+    //       layout: 'horizontal',
+    //       height: 48,
+    //       tagline: false,
+    //       shape: 'pill',
+    //     }}
+    //     createOrder={(data, actions) => {
+    //       return actions.order.create({
+    //         purchase_units: [
+    //           {
+    //             description: 'description',
+    //             amount: {
+    //               value: String(totalValue),
+    //             },
+    //           },
+    //         ],
+    //       });
+    //     }}
+    //     onApprove={async (data, actions: any) => {
+    //       console.log('approve', data);
+    //       try {
+    //         const newOrder = await createOrderData();
+    //         await actions.order.capture();
+    //         await setOrderDataPaypal(newOrder.id);
+    //       } catch (error) {}
 
-          // update order
+    //       // update order
 
-          console.log('approve finished');
-        }}
-        onError={(error) => {
-          console.log('error');
-        }}
-        onClick={() => {
-          // validate form / create order
-        }}
-      />
-    </PayPalScriptProvider>
-  );
+    //       console.log('approve finished');
+    //     }}
+    //     onError={(error) => {
+    //       console.log('error');
+    //     }}
+    //     onClick={() => {
+    //       // validate form / create order
+    //     }}
+    //   />
+    // </PayPalScriptProvider>
+  // );
 }
