ab456d2d5aded657f898865496aab4c24092c894
DamianBigos <damianbigos1500@gmail.com>
2023-04-03 18:33:06 +0200

    repair vercel bug

--- src/hooks/admin/useGetDataById.tsx
+++ src/hooks/admin/useGetDataById.tsx
@@ -4,7 +4,7 @@ import { useCallback, useEffect, useState } from 'react';
 export default function useGetDataById(
   name: string,
   getDataByIdService: (dataId: any) => Promise<AxiosResponse>,
-  dataId: number | null | string
+  dataId: number | null | string | string[]
 ) {
   const [data, setData] = useState<any>([]);
   const [loading, setLoading] = useState(true);
