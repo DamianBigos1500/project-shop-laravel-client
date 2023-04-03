37518ed6934127270a0e1a031b17919228d933a9
DamianBigos <damianbigos1500@gmail.com>
2023-04-03 18:43:49 +0200

    repair vercel bug

--- src/utils/createImgUrl.ts
+++ src/utils/createImgUrl.ts
@@ -1,6 +1,8 @@
 import { NO_IMAGE } from './../data/NO_IMAGE';
 
+const BACKEND_IMG_URL = 'https://project-shop-laravel-server-main.vercel.app';
+
 export const createImageUrl = (pathname: string | null = null) => {
   if (!pathname) return NO_IMAGE;
-  return process.env.NEXT_PUBLIC_BACKEND_IMG_URL + pathname;
+  return BACKEND_IMG_URL + pathname;
 };
