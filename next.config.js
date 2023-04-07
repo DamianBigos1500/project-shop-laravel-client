cf5b77ec776875ea881dd52f78366e2038e4ff08
DamianBigos <damianbigos1500@gmail.com>
2023-04-07 23:41:05 +0200

    vercel options

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: ['localhost', 'www.actbus.net', 'images.unsplash.com'],
+    domains: [
+      'localhost',
+      'www.actbus.net',
+      'images.unsplash.com',
+      'project-shop-laravel-server-main.vercel.app',
+    ],
   },
 };
